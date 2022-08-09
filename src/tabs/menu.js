040154d37ff5b99569f7d98ddc2eb516facf585e
Hamukaze <maplecow@gmail.com>
2022-08-09 16:58:16 +0900

    Added function to add menu items

---
+++ src/tabs/menu.js
@@ -0,0 +1,50 @@
+function createMenuItem(src, alt, name, desc, price){
+    const item = document.createElement('div');
+    item.classList.add('menuItem');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', src);
+    img.setAttribute('alt', alt);
+
+    const itemName = document.createElement('h3');
+    itemName.classList.add('itemName');
+    itemName.textContent = name;
+
+    const itemDesc = document.createElement('p')
+    itemDesc.classList.add('itemDesc');
+    itemDesc.textContent = desc;
+
+    const itemPrice = document.createElement('h2');
+    itemPrice.classList.add('itemPrice');
+    itemPrice.textContent = price;
+
+    item.appendChild(img);
+    item.appendChild(itemName);
+    item.appendChild(itemPrice);
+    item.appendChild(itemDesc);
+
+    return item;
+}
+
+//ideally the menu would come from a external file (maybe a csv of some sort) which pass through a few functions but for now this will be the method of menu creation
+
+function createMenu(){
+    const menu = document.createElement('section');
+    menu.classList.add('tabSection');
+
+    createMenuItem(
+        
+    )
+
+    return menu;
+}
+
+function loadMenu(){
+    const tabContent = document.getElementById('tabContent');
+    tabContent.innerHTML = '';
+
+    const menu = createMenu();
+
+    tabContent.appendChild(menu);
+
+}
