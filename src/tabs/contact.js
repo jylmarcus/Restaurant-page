271379adb4f51dd5c3e46934b007204096faa7e5
Hamukaze <maplecow@gmail.com>
2022-08-10 18:34:58 +0900

    Created contact page

---
+++ src/tabs/contact.js
@@ -0,0 +1,25 @@
+function createContact(){
+    const contact = document.createElement('section');
+    contact.classList.add('tabSection');
+
+    const phone = document.createElement('p');
+    phone.textContent = '📞7777777';
+
+    const address = document.createElement('p');
+    address.textContent = `Wherever you need us to be, kupo!`
+
+    phone.appendChild(phone);
+    address.appendChild(address);
+
+    return contact;
+}
+
+function loadContact(){
+    const tabContent = document.getElementById('tabContent');
+    tabContent.innerHTML = '';
+
+    const contact = createContact();
+
+    tabContent.appendChild(contact);
+
+}
